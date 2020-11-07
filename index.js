b346c4b99c58bbfa1f8618b7a6a6c433e9090a9e
jejaksaid <mail@saidramadhan.com>
2020-11-08 01:08:15 +0700

    0711

--- index.js
+++
@@ -1,5 +0,0 @@
-import React from   "react"
-import ReactDOM from "react-dom"
-
-// JSX
-ReactDOM.render(<div> <h1> This is Paragrapf</h1> <p>This is a paragraph</p></div> , document.getElementById ("root"))
