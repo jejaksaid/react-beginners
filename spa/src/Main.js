c127815c7863499ce647ffca33a844d959a20417
jejaksaid <mail@saidramadhan.com>
2020-11-08 02:23:21 +0700

    0811

--- spa/src/Main.js
+++ spa/src/Main.js
@@ -11,17 +11,22 @@ import Contact from "./Contact";
 class Main extends Component {
   render() {
     return (
-        <div>
-          <h1>Simple SPA</h1>
-          <ul className="header">
-            <li><a href="/">Home</a></li>
-            <li><a href="/stuff">Stuff</a></li>
-            <li><a href="/contact">Contact</a></li>
-          </ul>
-          <div className="content">
-             
-          </div>
-        </div>
+        <HashRouter>
+            <div>
+                <h1>Simple SPA</h1>
+                <ul className="header">
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/stuff">Stuff</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
+                </ul>
+                <div className="content">
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/Stuff" component={Stuff}/>
+                    <Route path="/Contact" component={Contact}/>
+                    
+                </div>
+            </div>
+        </HashRouter>
     );
   }
 }
