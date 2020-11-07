acbae6a4c8286d5c793dd8cccc698b2aa6234a5e
jejaksaid <mail@saidramadhan.com>
2020-11-08 03:58:18 +0700

    0811

---
+++ slidingmenu/src/MenuContainer.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class MenuContainer extends Component {
+    constructor(props, context) {
+        super(props, context);
+    
+        this.state = {
+            visible: false
+        };
+    
+        this.toggleMenu = this.toggleMenu.bind(this);
+        
+    }
+    
+    toggleMenu() {
+        this.setState({
+            visible: !this.state.visible
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <p>Can you spot the item that doesn't belong?</p>
+                    <ul>
+                        <li>Lorem</li>
+                        <li>Ipsum</li>
+                        <li>Dolor</li>
+                        <li>Sit</li>
+                        <li>Bumbleebes</li>
+                        <li>Aenan</li>
+                        <li>Constectur</li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
+export default MenuContainer;
