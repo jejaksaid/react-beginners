5a1094bc1464877df0c0bbde2283b89ed5d95534
jejaksaid <mail@saidramadhan.com>
2020-11-10 18:34:17 +0700

    1011

--- spa/src/Home.js
+++ spa/src/Home.js
@@ -4,13 +4,14 @@ class Home extends Component {
   render() {
     return (
       <div>
+        <section>
         <h2 class="home__title">Saidramadhan</h2>
         <span class="home__proffesion">Frontend Developer</span>
 
         <div>
           <a href="#" class="home__social-link"><i class="bx bxl-github" ></i>  </a>
-          <br>
-          </br><a href="#" class="home__social-link"><i class="bx bxl-linkedin"></i></a>
+          <a href="#" class="home__social-link"><i class="bx bxl-linkedin"></i></a>
+          <a href="#" class="home__social-link"><i class="bx bxl-dribbble"></i></a>
         </div>
         <p>Cras facilisis urna ornare ex volutpat, et
         convallis erat elementum. Ut aliquam, ipsum vitae
@@ -20,6 +21,7 @@ class Home extends Component {
         dis parturient montes, nascetur ridiculus mus.</p>
  
         <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+        </section>
       </div>
     );
   }
